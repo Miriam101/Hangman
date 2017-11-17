f75eb2627566bcb1f9bf7235e3aaef1e6b4ade26
shaikh <32182592+JetShaikh@users.noreply.github.com>
2017-11-16 16:43:56 -0800

    Update usarInput.js

--- usarInput.js
+++ usarInput.js
@@ -1,3 +1,4 @@
+//asking for user input
 function myFunction() {
     var txt;
     var letter = prompt("Please enter a letter:", "Letter");
@@ -9,19 +10,21 @@ function myFunction() {
     document.getElementById("demo").innerHTML = txt;
 }
 
+//setting var.
+var word = "Bread";
+var n = word.length;
+console.log(n);
 
-// var word = "Bread";
-// var n = word.length;
-// console.log(n);
-
-
+// unsure what id does figure it out later
 // var missedWord = ["Bread", "Plum", "Peach"];
-// // var n = missedWord.lenght(random);
-// // console.log(n);
+// var n = missedWord.lenght(random);
+// console.log(n);
 
 // var eRIKA = missedWord[Math.floor(Math.random()*items.length)];
 // console.log (eRIKA);
 
+
+//chooses random word 
 var NumberOfWords = 3
 
 var words = new BuildArray(NumberOfWords)
@@ -35,7 +38,7 @@ function BuildArray(size){
 this.length = size
 for (var i = 1; i <= size; i++){
 this[i] = null}
-console.log(this);
+// console.log(this);
 }
 
 function PickRandomWord(frm) {
@@ -46,7 +49,7 @@ frm.WordBox.value = words[rnd]
 }
 
 
-
+// counts letters in var word, use the blank spaces
 var letterInWord = function (letter, word) {
   var count = 0;
   for (var i = 0; i < word.length; i++) {
@@ -57,12 +60,33 @@ var letterInWord = function (letter, word) {
   return count;
 };
 
-function charcount(c, str) {
-  return str.replace(new RegExp('[^'+c+']','g'),'').length
+// function charcount(c, str) {
+//   return str.replace(new RegExp('[^'+c+']','g'),'').length
+// }
+
+// console.log = function(x) { document.write(x + "<br />"); };
+// console.log( "'*' in '4*5*6' = " + charcount('*', '4*5*6') ) // outputs 2
+// console.log( "'p' in 'pineapples' = " + charcount('p', 'pineapples') )// outputs 3
+
+// spells the word out induvidually 
+function letterCounter (x) {
+  return x.replace(/[^a-zA-Z]/g, '').length;
 }
 
-console.log = function(x) { document.write(x + "<br />"); };
-console.log( "'*' in '4*5*6' = " + charcount('*', '4*5*6') ) // outputs 2
-console.log( "'p' in 'pineapples' = " + charcount('p', 'pineapples') )// outputs 3
+console.log(letterCounter('Hello World!!!1'))
 
+var text = word;
+
+for(var x = 0, c=''; c = text.charAt(x); x++){ 
+    console.log(c); 
+}
+
+// code for serching through word looking for certain letter - using Booleans
+var letter = 'c';
+word.includes(letter); 
+  if (word.includes(letter)) {
+    console.log("true");
+  } else {
+    console.log("The word does not contain the letter " + letter);
+}
 
