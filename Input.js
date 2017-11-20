4b371b200be99eae519ca19dbf5d2c7a0a62c245
shaikh <32182592+JetShaikh@users.noreply.github.com>
2017-11-20 14:17:11 -0800

    Update Input.js

--- Input.js
+++ Input.js
@@ -12,23 +12,10 @@ var guessLetter = function(userLetter) {
 
 console.log(word.includes("a"));
 
-if (word.includes("a") === true) {
-  hiddenWord = hiddenWord.replace(/.3/g,"a");
-}
+// if (word.includes("a") === true) {
+//   hiddenWord = hiddenWord.replace(/.3/g,"a");
+// }
 
-if (userInput === 'b') {
-  console.log(hiddenWord = hiddenWord.replace(/....([^0-9])/, "b...."));
-} else if (userInput === 'r') {
-  console.log(hiddenWord = hiddenWord.replace(/....([^0-9])/, ".r..."));
-} else if (userInput === 'e') {
-  console.log(hiddenWord = hiddenWord.replace(/....([^0-9])/, "..e.."));
-} else if (userInput === 'a'){
-  console.log(hiddenWord = hiddenWord.replace(/....([^0-9])/, "...a."));
-} else if (userInput === 'd') {
-  console.log(hiddenWord = hiddenWord.replace(/....([^0-9])/, "....d"));
-} else {
-  console.log("Sorry thats invalid letter");
-  numberGuesses = numberGuesses - 1;
-}
-
-console.log(hiddenWord);
+var word = "bread";
+    substring = userInput;
+word.indexOf(userInput);
